93e6bdf6d05c650a0dc57858e5e893bbc26fe319
Ed Salisbury <ed@edsalisbury.net>
2012-10-02 17:58:01 -0600

    Added book edit functionality

--- book/urls.py
+++ book/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls import patterns, include, url
 urlpatterns = patterns('book.views',
     url(r'^$', 'list'),
     url(r'^/(?P<book_id>\d+)$', 'view'),
+    url(r'^/(?P<book_id>\d+)/edit$', 'edit'),
     url(r'^/add$', 'add'),
     url(r'^/mass_add$', 'mass_add'),
 )
--- book/views.py
+++ book/views.py
@@ -9,6 +9,9 @@ import HTMLParser
 from types import *
 import re
 import inspect
+import logging
+
+logger = logging.getLogger(__name__)
 
 def list(request):
     book_list = Book.objects.all().order_by('title').order_by('publication_year').order_by('author__name')
@@ -22,7 +25,104 @@ def view(request, book_id):
     book = get_object_or_404(Book, pk=book_id)
     img_url = settings.MEDIA_URL + "book/" + str(book.id) + ".jpg"
     book.description = book.description.replace("\\'", "'")
-    return render_to_response('book/view.html', {'book': book, 'img_url': img_url, 'tab': 'books'})
+    return render_to_response('book/view.html', {'book': book, 'img_url': img_url, 'tab': 'books', 'action': 'view'})
+
+def get_author(name):
+    name = unicode(name).encode('utf-8')
+    try:
+        author = Author.objects.get(name = name)
+    except Author.DoesNotExist:
+        author = Author.objects.create(name = name)
+    return author
+
+def get_publisher(name):
+    name = unicode(name).encode('utf-8')
+    try:
+        publisher = Publisher.objects.get(name = name)
+    except Publisher.DoesNotExist:
+        publisher = Publisher.objects.create(name = name)
+    return publisher
+
+def get_series(name):
+    if not name:
+        return None
+    name = unicode(name).encode('utf-8')
+    try:
+        series = Series.objects.get(name = name)
+    except Series.DoesNotExist:
+        series = Series.objects.create(name = name)
+    return series
+
+def get_binding_by_id(binding_id):
+    binding = None
+    try:
+        binding = Format.objects.get(pk = binding_id)
+    except Format.DoesNotExist:
+        pass
+
+    return binding
+
+def get_binding_by_name(name):
+    name = unicode(name).encode('utf-8')
+    try:
+        binding = Format.objects.get(label = name)
+    except Format.DoesNotExist:
+        binding = Format.objects.create(label = name)
+    return binding
+
+def get_condition_by_id(condition_id):
+    condition = None
+    try:
+        condition = Condition.objects.get(pk = condition_id)
+    except Condition.DoesNotExist:
+        pass
+    return condition
+
+def get_condition_by_name(name):
+    name = unicode(name).encode('utf-8')
+    try:
+        condition = Condition.objects.get(label = name)
+    except Condition.DoesNotExist:
+        condition = Condition.objects.create(label = name)
+    return condition
+
+def edit(request, book_id):
+    c = {}
+    c['tab'] = 'books'
+    c['action'] = 'edit'
+    c.update(csrf(request))
+
+    if request.method == 'POST':
+        post = request.POST
+        logger.debug(post)
+        title = unicode(post['title']).encode('utf-8')
+        author = get_author(post['author'])
+        series = get_series(post['series'])
+        series_number = post['series_number']
+        binding = get_binding_by_name(post['binding'])
+        condition = get_condition_by_name(post['condition'])
+        description = unicode(post['description']).encode('utf-8')
+        publisher = get_publisher(post['publisher'])
+        isbn = post['isbn']
+
+        book = Book.objects.get(pk = book_id)
+        book.title = title
+        book.author = author
+        book.series = series
+        book.series_number = series_number
+        book.format = binding
+        book.condition = condition
+        book.description = description
+        book.publisher = publisher
+        book.isbn = isbn
+
+        book.save()
+        c['updated'] = True
+
+    c['book'] = get_object_or_404(Book, pk=book_id)
+    c['bindings'] = Format.objects.all()
+    c['conditions'] = Condition.objects.all()
+    return render_to_response('book/edit.html', c)
 
 def add(request):
     return render_to_response('book/add.html', {'tab': 'books'})
