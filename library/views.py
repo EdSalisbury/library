6e7372e1b0f1250f33a11a173f76563f562b3a36
Ed Salisbury <ed@edsalisbury.net>
2012-10-15 14:29:05 -0600

    Internalized static files

--- library/views.py
+++ library/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render_to_response, get_object_or_404
 
 def index(request):
     return render_to_response('index.html')
+
+def login(request):
+    return render_to_response("login.html")
